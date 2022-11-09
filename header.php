e0c0a32b2ab4538d38c17af8a24bbacd20b45bb7
Ahrusalome <90481871+Ahrusalome@users.noreply.github.com>
2022-11-09 15:55:32 +0100

    deconnect session

--- header.php
+++ header.php
@@ -1,8 +1,18 @@
 <div class="header">
-    <div class="leftHeader">
+    <div id="leftHeader">
         <p>left Header </p>
     </div>
-    <div class="rightHeader">
+    <div id="rightHeader">
         <p>right Header </p>
     </div>
+    <form method="POST" id="deconnexionForm">
+        <input type="submit" name="deconnect" value="deconnexion">
+    </form>
+    <?php 
+        if (isset($_POST["deconnect"])) {
+            session_unset();
+            session_destroy();
+            header('Location: signIn.php');
+        }
+    ?>
 </div>
