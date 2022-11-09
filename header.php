b5e5d8fe2a388b2a0d4d50e323f1519c539ffcec
Ahrusalome <90481871+Ahrusalome@users.noreply.github.com>
2022-11-09 15:59:11 +0100

    better deco

--- header.php
+++ header.php
@@ -5,10 +5,10 @@
     <div id="rightHeader">
         <p>right Header </p>
     </div>
-    <form method="POST" id="deconnexionForm">
-        <input type="submit" name="deconnect" value="deconnexion">
-    </form>
     <?php 
+        if (session_status() === PHP_SESSION_ACTIVE){
+            echo "<form method=POST id=deconnexionForm><input type=submit name=deconnect value=deconnexion></form>";
+        }
         if (isset($_POST["deconnect"])) {
             session_unset();
             session_destroy();
